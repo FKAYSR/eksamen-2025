2fcafd9cfb4cdc4015d7aca798b63e6a8f6c41b3
FKAYSR <kejserfreja@gmail.com>
2025-11-25 16:37:43 +0100

    Lyd + errors
    Fixet de errors der var fra HTML validatoren.
    
    Ændrede også kilderne til lyd-filerne så lydende virker.
    
    Fjernede "ekstra" knappen.

--- eksamens_projekt_2025/js/script.js
+++ eksamens_projekt_2025/js/script.js
@@ -1,49 +1,35 @@
 "use strict";
 
-// funktion der afspiller lyd når boble klikkes og tilføjer pop animation
+// Funktion der afspiller lyd når boble klikkes
 function handleBubbleClick(event) {
-  const bubbleSound = new Audio("exd-akvarie/sound/bubble-pop.mp3");
+  const bubbleSound = new Audio("sound/forside/bubble-pop.mp3");
   bubbleSound.play();
 }
 
-// lav den nu med skattekisten, når man klikker på den så skal der afspilles en lyd af skattekisten der åbnes, med skattekiste-sounden
-function handleSkattekisteClick(event) {
-  const skattekisteSound = new Audio("exd-akvarie/sound/skattekiste-sound.mp3");
-  skattekisteSound.play();
-}
-
-// venter med at hente js siden tilden hele html er loaded
+// venter med at hente js-siden til hele html er loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Add click handlers to all bubbles
+  // tilføjer click handlers til alle bubbles
   document.querySelectorAll(".bubble").forEach((bubble) => {
     bubble.addEventListener("click", handleBubbleClick);
   });
 });
 
-// venter med at hente js siden tilden hele html er loaded
+
+// Skattekiste lyd
+function handleSkattekisteClick(event) {
+  const skattekisteSound = new Audio("sound/forside/skattekiste-sound.mp3");
+  skattekisteSound.play();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Add click handler to skattekiste
   const skattekiste = document.getElementById("skattekiste");
   skattekiste.addEventListener("click", handleSkattekisteClick);
 });
 
-function playBtnClick(event) {
-  const playBtnSound = new Audio("exd-akvarie/sound/eating-chips.mp3");
-  playBtnSound.play();
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const playBtn = document.getElementById("play-button");
-  playBtn.addEventListener("click", playBtnClick);
-});
-
-// Event listener til play-knappen for at navigere til spil siden
-document.getElementById("play-button").addEventListener("click", function () {
-  window.location.href = "../index-spil.html";
-});
-
+// Piratskib lyd
 function piratClick(event) {
-  const piratSound = new Audio("exd-akvarie/sound/wooden-ship-break.mp3");
+  const piratSound = new Audio("sound/forside/wooden-ship-break.mp3");
   piratSound.play();
 }
 
@@ -52,7 +38,16 @@ document.addEventListener("DOMContentLoaded", () => {
   piratskib.addEventListener("click", piratClick);
 });
 
-// Tilføj event listener til knappen for at navigere til fakta siden
-document.getElementById("fish-button").addEventListener("click", function () {
-  window.location.href = "../index-facts.html";
-});
+
+// #region - Virker ikke endnu! Lyd til spil knappen
+// function playBtnClick(event) {
+//   const playBtnSound = new Audio("exd-akvarie/sound/eating-chips.mp3");
+//   playBtnSound.play();
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const playBtn = document.getElementById("play-button");
+//   playBtn.addEventListener("click", playBtnClick);
+// });
+
+// #endregion
